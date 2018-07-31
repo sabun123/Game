6edbf6a77c63b3830a0fdde957f1bea02917e945
yusuf <linkinpark_neon@yahoo.com>
2018-07-31 17:41:21 +0800

    Random colored cubes
    Added random color to cubes that drop and also a catcher to be used later for "catching" the cubes.

--- Blessings Never Last/Assets/Scripts/Spawner.cs
+++ Blessings Never Last/Assets/Scripts/Spawner.cs
@@ -8,6 +8,9 @@ public class Spawner : MonoBehaviour
     public GameObject objectToSpawn;
     public float timeTillFirstWave = 1f;
     public float timeToNewWave = 2f;
+    public GameObject angleOfSpawn;
+    public GameObject point1;
+    public GameObject point2;
 
     // Private
     List<GameObject> listOfRandomSpawnedObjects;
@@ -28,13 +31,36 @@ public class Spawner : MonoBehaviour
 
     void CreateSpawnablesLoop()
     {
-
+        // Grab a random point anywhere on the plane
         int randomNum = Random.Range(1, 10);
+        // Angle at which we want to point the spawned game objects at
+        Quaternion angleToAimAt = Quaternion.LookRotation(Vector3.forward, Vector3.up);
         for (int count = 0; count < randomNum; count++)
         {
-            GameObject newSpawn = Instantiate(objectToSpawn, new Vector3(0, 0, 0), Quaternion.identity);
+            float xCoord = GetRandomXCoordinateFromObject();
+            float zCoord = GetRandomZCoordinateFromObject();
+            Debug.Log("X " + xCoord + " Z " + zCoord);
+            GameObject newSpawn = Instantiate(objectToSpawn, new Vector3(xCoord, gameObject.transform.localPosition.y, zCoord), angleToAimAt);
             listOfRandomSpawnedObjects.Add(newSpawn);
         }
+    }
+
+    float GetRandomXCoordinateFromObject()
+    {
+        //float x1 = gameObject.transform.position.x - gameObject.transform.localScale.x / 2;
+        //float x2 = gameObject.transform.position.x + gameObject.transform.localScale.x / 2;
+        float x1 = point1.transform.position.x; 
+        float x2 = point2.transform.position.x;
+        return Random.Range(x1,x2);
+    }
+
+    float GetRandomZCoordinateFromObject()
+    {
+        //float z1 = gameObject.transform.position.z - gameObject.transform.localScale.z / 2;
+        //float z2 = gameObject.transform.position.z + gameObject.transform.localScale.z / 2;
+        float z1 = point1.transform.position.z;
+        float z2 = point2.transform.position.z;
+        return Random.Range(z1, z2);
     }
 
     public void CleanUp(GameObject reference)
