4a8e192e9fd3459c3ab7631462d80320a2deda81
yusuf <linkinpark_neon@yahoo.com>
2018-07-31 15:06:54 +0800

    Initial Spawner System
    Spawn random number of game objects and have them self clean up after set period of time.

---
+++ Blessings Never Last/Assets/Scripts/SelfDestroyTimer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfDestroyTimer : MonoBehaviour {
+
+    /*  Simple script to determine how long to live in the scene
+     *  then perform self cleanup once time is up
+     */
+
+    public GameObject instanceOfSpawner;
+    public float lifeSpan;
+
+	// Use this for initialization
+	void Start () {
+        // Find the spawn plane game object from the scene
+        instanceOfSpawner = GameObject.FindGameObjectWithTag("SpawnPlane");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        lifeSpan -= Time.deltaTime;
+        if(lifeSpan < 0)
+        {
+            // If this object has lived longer than it should have
+            // Time to delete it from memory then
+            selfCleanUp();
+        }
+	}
+
+    void selfCleanUp()
+    {
+        // Self cleanup
+        // Grab instance of script on the Plane Spawner gameObject
+        Spawner instance = instanceOfSpawner.GetComponent<Spawner>();
+        // Call the cleanup function from that script
+        instance.CleanUp(gameObject);
+    }
+
+}
---
+++ Blessings Never Last/Assets/Scripts/Spawner.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    // Visible in Unity UI
+    public GameObject objectToSpawn;
+    public float timeTillFirstWave = 1f;
+    public float timeToNewWave = 2f;
+
+    // Private
+    List<GameObject> listOfRandomSpawnedObjects;
+
+    // Use this for initialization
+    void Start()
+    {
+        listOfRandomSpawnedObjects = new List<GameObject>();
+
+        // Create a new wave of spawnables every timeToNewWave (second) starting in two seconds
+        InvokeRepeating("CreateSpawnablesLoop", timeTillFirstWave, timeToNewWave);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void CreateSpawnablesLoop()
+    {
+
+        int randomNum = Random.Range(1, 10);
+        for (int count = 0; count < randomNum; count++)
+        {
+            GameObject newSpawn = Instantiate(objectToSpawn, new Vector3(0, 0, 0), Quaternion.identity);
+            listOfRandomSpawnedObjects.Add(newSpawn);
+        }
+    }
+
+    public void CleanUp(GameObject reference)
+    {
+        if (listOfRandomSpawnedObjects.Contains(reference))
+        {
+            // This gameobject is about to be destroyed, remove it from list
+            listOfRandomSpawnedObjects.Remove(reference);
+            // Now destroy it (cleans up memory too)
+            Destroy(reference);
+        }
+    }
+}
