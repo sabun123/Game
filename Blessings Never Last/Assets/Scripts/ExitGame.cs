67252b19d3e319d5b81d0a5dec96cedb2efffc8b
yusuf <linkinpark_neon@yahoo.com>
2018-08-02 18:03:23 +0800

    Main menu added
    Quick main menu added pending further additions. Allows starting game and exiting, with placeholder for a proper loading screen.

---
+++ Blessings Never Last/Assets/Scripts/ExitGame.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class ExitGame : MonoBehaviour {
+
+    Button exit;
+
+	// Use this for initialization
+	void Start () {
+        // Grab the button component of this game object
+        exit = gameObject.GetComponent<Button>();
+        // Add a listener function for the onclick event
+        exit.onClick.AddListener(OnClick);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnClick()
+    {
+        Application.Quit();
+    }
+}
---
+++ Blessings Never Last/Assets/Scripts/PlayNewGame.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class PlayNewGame : MonoBehaviour {
+
+    Button thisButton;
+    public GameObject loadingPanel;
+    public int sceneNumber;
+
+	// Use this for initialization
+	void Start () {
+        // Grab the button component off this UI element
+        // that this script is attached to
+        thisButton = gameObject.GetComponent<Button>();
+        // Attach a listener
+        thisButton.onClick.AddListener(OnClick);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnClick()
+    {
+        // Show the loading screen panel
+        loadingPanel.SetActive(true);
+        // Fire off the scene loading behind the scenes
+        StartCoroutine(LoadNewScene());
+    }
+
+    // Coroutine for loading
+    IEnumerator LoadNewScene()
+    {
+        //AsyncOperation async = Application.LoadLevelAsync(sceneNumber);
+        AsyncOperation async = SceneManager.LoadSceneAsync(sceneNumber);
+
+        // While the scene is still loading, keep repeating this coroutine running.
+        while (!async.isDone)
+        {
+            yield return null;
+        }
+    }
+}
